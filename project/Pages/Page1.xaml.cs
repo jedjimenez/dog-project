bc9dac124641ec0e50ae8704cf1fc9b10e8a0b24
leo velasquez <leo12v@hotmail.com>
2020-03-14 18:32:58 -0700

    binding

--- project/Pages/Page1.xaml.cs
+++ project/Pages/Page1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,20 @@ using Xamarin.Forms.Xaml;
 
 namespace project.Pages
 {
+    public class information
+    {
+        public string text { get; set; }
+        public string smallprint { get; set; } 
+        public string image { get; set; }
+    }
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
+        public ObservableCollection<information> list1 { get; set; } //makes an observable collection for the cells
         public Page1()
         {
             InitializeComponent();
         }
+
     }
 }
